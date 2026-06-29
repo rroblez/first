2a88d01cfdfd153a28bfa6479e546df0745df91d
rroblez <71054485+rroblez@users.noreply.github.com>
2026-06-29 10:22:23 -0700

    Remove redundant warning filter for NotOpenSSLWarning

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import warnings
 from urllib3.exceptions import NotOpenSSLWarning
-
-warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
-
 import requests
 import pynetbox
 
+warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
 print ('hi')
 
